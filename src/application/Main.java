7c44245a08ef779d9ed651e7224bce072ce3dfa3
Murilo Damario <murilodamario1@gmail.com>
2021-07-07 22:48:25 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,13 +2,17 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
         System.out.println(obj);
     }
 }
