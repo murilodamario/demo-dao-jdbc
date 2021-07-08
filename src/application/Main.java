5dd87ee3f499f826d730ec6e562a5096f109432c
Murilo Damario <murilodamario1@gmail.com>
2021-07-07 22:38:06 -0300

    Department entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,14 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
 public class Main {
 
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
     }
 }
